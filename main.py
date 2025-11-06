721533332dce86f2d38244dd8b463a91a598b5b2
Aleksei Loguntsov <loguntsovae@gmail.com>
2025-11-06 13:33:22 +0400

    Initial

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine, text
 
-DATABASE_URL = "postgresql://user:password@db-endpoint:5432/demo"
+DATABASE_URL = "postgresql://postgres:postgres@demo-db.c7umuy8uul98.eu-central-1.rds.amazonaws.com:5432/demo"
 engine = create_engine(DATABASE_URL)
 
 app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"status": "ok"}
 
 @app.get("/users")
 def get_users():
